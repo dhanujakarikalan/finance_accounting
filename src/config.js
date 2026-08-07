5353748d21246eb57a3efd05bdbe1661019b7517
Dhanuja <dhanuja@example.com>
2026-08-07 09:45:04 +0530

    UpdateProductionWebhookURL

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const WEBHOOK_URL = "https://api.agents.snsihub.ai/webhook-test/b80bf861-476f-405f-9e85-3d7da5fda821";
+export const WEBHOOK_URL = "https://api.agents.snsihub.ai/webhook/b80bf861-476f-405f-9e85-3d7da5fda821";
 export const API_BASE_URL = "http://localhost:8000";
 
